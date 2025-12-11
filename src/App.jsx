388f1214cedb3bd56a87cd910ee642229fe17c01
Tanuja Gunapooti <g.tanuja@wustl.edu>
2025-12-11 16:11:07 -0600

    fixed nav for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route, NavLink } from "react-router-dom";
 import "./App.css";
+import { useState } from "react";
 import Home from "./pages/Home.jsx";
 import Guide from "./pages/Guide.jsx";
 import AboutMe from "./pages/AboutMe.jsx";
@@ -7,52 +8,34 @@ import Analysis from "./pages/Analysis.jsx";
 import Citations from "./pages/Citations.jsx";
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="app">
       {/* NAVBAR */}
+
       <nav className="navbar">
-        <div className="nav-links">
-          <NavLink
-            to="/"
-            end
-            className={({ isActive }) =>
-              "nav-link" + (isActive ? " active" : "")
-            }
-          >
+        <button className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+          ☰
+        </button>
+
+        <div className={`nav-links ${menuOpen ? "open" : ""}`}>
+          <NavLink to="/" end className="nav-link">
             Home
           </NavLink>
 
-          <NavLink
-            to="/guide"
-            className={({ isActive }) =>
-              "nav-link" + (isActive ? " active" : "")
-            }
-          >
+          <NavLink to="/guide" className="nav-link">
             General
           </NavLink>
 
-          <NavLink
-            to="/analysis"
-            className={({ isActive }) =>
-              "nav-link" + (isActive ? " active" : "")
-            }
-          >
+          <NavLink to="/analysis" className="nav-link">
             Granny Square vs Amigurumi
           </NavLink>
-          <NavLink
-            to="/citation"
-            className={({ isActive }) =>
-              "nav-link" + (isActive ? " active" : "")
-            }
-          >
-            Work Citied
+
+          <NavLink to="/citation" className="nav-link">
+            Work Cited
           </NavLink>
-          <NavLink
-            to="/about"
-            className={({ isActive }) =>
-              "nav-link" + (isActive ? " active" : "")
-            }
-          >
+
+          <NavLink to="/about" className="nav-link">
             About Me
           </NavLink>
         </div>
